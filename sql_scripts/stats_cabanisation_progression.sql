04bc2554f550b7e3e19f8b6e378f8e35276c2d25
raphael deckers <deckers.raphael@gmail.com>
2025-07-08 15:18:37 +0200

    feat: scripts stats evolution

--- sql_scripts/stats_cabanisation_progression.sql
+++ sql_scripts/stats_cabanisation_progression.sql
@@ -1,169 +1,188 @@
+-- create stats tables : 
+-- DROP TABLE analytics.stats_control_analysis;
 
+CREATE TABLE analytics.stats_control_analysis (
+	id serial4 NOT NULL,
+	geozone_id int4 NOT NULL,
+	"year" int4 NOT NULL,
+	nb_controls int4 NOT NULL
+);
 
--- Step 1: Créer la table d analyse
-CREATE TABLE IF NOT EXISTS detections.tmp_detections_analysis (
-    id                 BIGINT PRIMARY KEY,
-    score              NUMERIC,
-    object_type        TEXT,
-    geometry           geometry(MultiPolygon, 4326),
-    geocustomzone_id   NUMERIC,
-    batch_year   NUMERIC,
-    id_parcellaire     TEXT,
-    first_detection    BOOLEAN
+CREATE TABLE analytics.stats_detections_analysis (
+	id int8 NOT NULL,
+	score numeric NULL,
+	object_type text NULL,
+	geometry public.geometry(multipolygon, 4326) NULL,
+	geozone_id numeric NULL,
+	batch_year numeric NULL,
+	id_parcellaire text NULL,
+	first_detection bool NULL
+);
+
+CREATE TABLE analytics.stats_parcels_analysis (
+	id serial4 NOT NULL,
+	geozone_id int4 NOT NULL,
+	nb_parcels int4 NOT NULL,
+	nb_parcels_in_ze int4 NOT NULL,
+	CONSTRAINT stats_parcels_analysis_pkey PRIMARY KEY (id)
 );
 
 
--- Step 2: Inserer les données calculées et agrégées
-WITH params AS (
-    SELECT {geocustomzone_id}::numeric AS commune_geozone_id, {current_commune_batch_id}::numeric AS commune_batch_id
-),
-batch_data_to_insert AS 
-(
-SELECT DISTINCT ON (inf.id)
-    inf.id,
-    inf.score,
-    CASE 
-        WHEN inf.object_type IN ('construction en dur', 'construction legere yourte etc.', 'container', 'installation legere') THEN 'construction en dur'
-        ELSE inf.object_type
-    END AS object_type,
-    inf.geometry,
-    p.commune_geozone_id AS geocustomzone_id,
-    p.commune_batch_id AS commune_batch_id,
-    CASE 
-        WHEN commune_batch_id = {2018_commune_batch_id} THEN 2018
-        WHEN commune_batch_id = {2021_commune_batch_id} THEN 2021
-        when commune_batch_id = {2024_commune_batch_id} THEN 2024
-        ELSE null
-    END as batch_year,
-    cp.id_parcellaire
-FROM
-    detections.inference inf 
-    JOIN params p ON inf.batch_id = p.commune_batch_id
-    LEFT JOIN core_parcel cp 
-        ON ST_Intersects(cp.geometry, inf.geometry)
-WHERE
-    inf.score > 0.3
-    and
-    ST_Within(ST_SetSRID(inf.geometry, 4326), (
-        SELECT geozone.geometry
-        FROM core_geozone geozone
-        WHERE geozone.id = p.commune_geozone_id
-    ))
-    AND inf.object_type IN (
-        'construction en dur',
-        'piscine',
-        'mobil home',
-        'caravane',
-        'construction legere yourte etc.',
-        'container',
-        'installation legere'
-    )
-    and 
-   (
-   ST_Within(ST_SetSRID(inf.geometry, 4326), (
-        SELECT geozone.geometry
-        FROM core_geozone geozone
-        WHERE geozone.id = 361 -- zone enjeu env herault
-    ))
-   or 
-   ST_Within(ST_SetSRID(inf.geometry, 4326), (
-        SELECT geozone.geometry
-        FROM core_geozone geozone
-        WHERE geozone.id = 352 -- zone risque fort herault
-    ))
-    or 
-   ST_Within(ST_SetSRID(inf.geometry, 4326), (
-        SELECT geozone.geometry
-        FROM core_geozone geozone
-        WHERE geozone.id = 351 -- zone naturelle et agricole herault
-    ))
-   )
-   ORDER BY inf.id, cp.id  -- garder seulement la premiere parcelle associée a chaque detection
-)
-INSERT INTO detections.tmp_detections_analysis (
-    id,
-    score,
-    object_type,
-    geometry,
-    geocustomzone_id,
-    batch_year,
-    id_parcellaire,
-    first_detection
-)
-SELECT 
-    t.id,
-    t.score,
-    t.object_type,
-    t.geometry,
-    t.geocustomzone_id,
-    t.batch_year,
-    t.id_parcellaire,
-    NOT EXISTS (
-        SELECT 1
-        FROM detections.tmp_detections_analysis existing
-        WHERE 
-            ST_Intersects(t.geometry, existing.geometry)
-            AND ST_Area(ST_Intersection(t.geometry, existing.geometry)) / ST_Area(t.geometry) > 0.5
-    ) AS first_detection
-FROM batch_data_to_insert t;
-;
+CREATE TABLE analytics.stats_pv_analysis (
+	id int8 NOT NULL,
+	geozone_id numeric NULL,
+	"year" numeric NULL,
+	nb_pv numeric NULL
+);
 
--- Step 3 : Extraire les infos sur les données construites
--- /!\ parametres geocustomzone_id, et attention pour les évolutions (nb nouvelles parcelles cabanisées, nb parcelles remises en état) il faut saisir les années a comparer 
 
--- nb objets detectés par categories
-select object_type,batch_year, count(*) from detections.tmp_detections_analysis
-where geocustomzone_id = {geocustomzone_id}
-group by object_type,batch_year 
-order by batch_year ;
+CREATE TABLE analytics.stats_status_analysis (
+	id serial4 NOT NULL,
+	geozone_id int4 NOT NULL,
+	"year" int4 NOT NULL,
+	nb_status_updates int4 NOT NULL
+);
 
--- nb parcelles cabanisées
-select batch_year, count(distinct(id_parcellaire)) from detections.tmp_detections_analysis
-where geocustomzone_id = {geocustomzone_id}
-group by batch_year 
-order by batch_year ;
+-- use notebook to build intermediate geozone aigle stats
 
--- nb parcelles avec des nouvelles detection
-select batch_year, count(distinct(id_parcellaire)) from detections.tmp_detections_analysis
-where geocustomzone_id = {geocustomzone_id} and first_detection = true
-group by batch_year 
-order by batch_year ;
+-- create view for metabase dashboards
 
--- nb nouvelles parcelles cabanisées
-SELECT count(*) FROM (
-	SELECT DISTINCT id_parcellaire
-	FROM detections.tmp_detections_analysis
-	WHERE batch_year = 2021
-	and geocustomzone_id = {geocustomzone_id}
-	  AND id_parcellaire IS NOT NULL
-	EXCEPT
-	SELECT DISTINCT id_parcellaire
-	FROM detections.tmp_detections_analysis
-	WHERE batch_year = 2018
-	and geocustomzone_id = {geocustomzone_id}
-	  AND id_parcellaire IS NOT null
-	 );
-	
--- liste ex : -- cas interessant nouvelle parcelle cabanisée :'34032000CN0116'
-select id_parcellaire, batch_year, count(*) from tmp_detections_analysis tda	where id_parcellaire in ('34032000CN0116','34032000BK0084','34032000DV0276','34032000AT0200','34032000BZ0273','34032000AV0081','34032000CT0162')
-group by id_parcellaire, batch_year;
-	
+-- analytics.stats_evolutions_cabanisation source
 
--- nb parcelles remises en état entre 2018 et 2021
--- selectionner la liste des parcelles cabanisees de 2018 n'etant pas présentes dans la liste parcelles des cabanisées 2021
-SELECT count(DISTINCT t2018.id_parcellaire)
-FROM detections.tmp_detections_analysis t2018
-WHERE t2018.batch_year = 2018
-  AND t2018.id_parcellaire IS NOT null
-  and t2018.geocustomzone_id = {geocustomzone_id}
-  AND NOT EXISTS (
-    SELECT 1
-    FROM detections.tmp_detections_analysis t2021
-    WHERE t2021.batch_year = 2021
-      and t2021.geocustomzone_id = {geocustomzone_id}
-      AND t2021.id_parcellaire = t2018.id_parcellaire
-  );
- 
- -- liste ex : -- cas interessant parcelle remise en état '34032000AM0026'
-select id_parcellaire, batch_year, count(*) from tmp_detections_analysis tda	where id_parcellaire in ('34032000AB0015','34032000AK0007','34032000AM0026','34032000AM0030','34032000AN0042','34032000AN0049','34032000AS0038')
-group by id_parcellaire, batch_year;
+CREATE OR REPLACE VIEW analytics.stats_evolutions_cabanisation
+AS WITH stats_pv_2018_2020 AS (
+         SELECT stats_pv_analysis.geozone_id,
+            sum(
+                CASE
+                    WHEN stats_pv_analysis.year >= 2018::numeric AND stats_pv_analysis.year <= 2020::numeric THEN stats_pv_analysis.nb_pv
+                    ELSE 0::numeric
+                END) AS nb_pv_2018_2020
+           FROM analytics.stats_pv_analysis
+          GROUP BY stats_pv_analysis.geozone_id
+          ORDER BY stats_pv_analysis.geozone_id
+        ), stats_pv_2021_2023 AS (
+         SELECT stats_pv_analysis.geozone_id,
+            sum(
+                CASE
+                    WHEN stats_pv_analysis.year >= 2021::numeric AND stats_pv_analysis.year <= 2023::numeric THEN stats_pv_analysis.nb_pv
+                    ELSE 0::numeric
+                END) AS nb_pv_2021_2023
+           FROM analytics.stats_pv_analysis
+          GROUP BY stats_pv_analysis.geozone_id
+          ORDER BY stats_pv_analysis.geozone_id
+        ), stats_parcelles_cabanisees_2018 AS (
+         SELECT stats_detections_analysis.geozone_id,
+            count(DISTINCT stats_detections_analysis.id_parcellaire) AS situation_parcelles_cabanisees_2018
+           FROM analytics.stats_detections_analysis
+          WHERE stats_detections_analysis.batch_year = 2018::numeric
+          GROUP BY stats_detections_analysis.geozone_id
+          ORDER BY stats_detections_analysis.geozone_id
+        ), stats_parcelles_cabanisees_2021 AS (
+         SELECT stats_detections_analysis.geozone_id,
+            count(DISTINCT stats_detections_analysis.id_parcellaire) AS situation_parcelles_cabanisees_2021
+           FROM analytics.stats_detections_analysis
+          WHERE stats_detections_analysis.batch_year = 2021::numeric
+          GROUP BY stats_detections_analysis.geozone_id
+          ORDER BY stats_detections_analysis.geozone_id
+        ), stats_parcelles_cabanisees_2024 AS (
+         SELECT stats_detections_analysis.geozone_id,
+            count(DISTINCT stats_detections_analysis.id_parcellaire) AS situation_parcelles_cabanisees_2024
+           FROM analytics.stats_detections_analysis
+          WHERE stats_detections_analysis.batch_year = 2024::numeric
+          GROUP BY stats_detections_analysis.geozone_id
+          ORDER BY stats_detections_analysis.geozone_id
+        ), stats_nouvelles_parcelles_cabanisees_2021 AS (
+         SELECT new_parcellaire.geozone_id,
+            count(*) AS nouvelles_parcelles_cabanisees_2021_vs_2018
+           FROM ( SELECT DISTINCT stats_detections_analysis.id_parcellaire,
+                    stats_detections_analysis.geozone_id
+                   FROM analytics.stats_detections_analysis
+                  WHERE stats_detections_analysis.batch_year = 2021::numeric AND stats_detections_analysis.id_parcellaire IS NOT NULL
+                EXCEPT
+                 SELECT DISTINCT stats_detections_analysis.id_parcellaire,
+                    stats_detections_analysis.geozone_id
+                   FROM analytics.stats_detections_analysis
+                  WHERE stats_detections_analysis.batch_year = 2018::numeric AND stats_detections_analysis.id_parcellaire IS NOT NULL) new_parcellaire
+          GROUP BY new_parcellaire.geozone_id
+          ORDER BY new_parcellaire.geozone_id
+        ), stats_nouvelles_parcelles_cabanisees_2024 AS (
+         SELECT new_parcellaire.geozone_id,
+            count(*) AS nouvelles_parcelles_cabanisees_2024_vs_2021
+           FROM ( SELECT DISTINCT stats_detections_analysis.id_parcellaire,
+                    stats_detections_analysis.geozone_id
+                   FROM analytics.stats_detections_analysis
+                  WHERE stats_detections_analysis.batch_year = 2024::numeric AND stats_detections_analysis.id_parcellaire IS NOT NULL
+                EXCEPT
+                 SELECT DISTINCT stats_detections_analysis.id_parcellaire,
+                    stats_detections_analysis.geozone_id
+                   FROM analytics.stats_detections_analysis
+                  WHERE (stats_detections_analysis.batch_year = ANY (ARRAY[2021::numeric, 2018::numeric])) AND stats_detections_analysis.id_parcellaire IS NOT NULL) new_parcellaire
+          GROUP BY new_parcellaire.geozone_id
+          ORDER BY new_parcellaire.geozone_id
+        ), stats_parcelles_remises_en_etat_2021 AS (
+         SELECT t2018.geozone_id,
+            count(DISTINCT t2018.id_parcellaire) AS parcelles_remises_en_etat_2021_vs_2018
+           FROM analytics.stats_detections_analysis t2018
+          WHERE t2018.batch_year = 2018::numeric AND t2018.id_parcellaire IS NOT NULL AND NOT (EXISTS ( SELECT 1
+                   FROM analytics.stats_detections_analysis t2021
+                  WHERE t2021.batch_year = 2021::numeric AND t2021.id_parcellaire = t2018.id_parcellaire AND t2021.geozone_id = t2018.geozone_id))
+          GROUP BY t2018.geozone_id
+          ORDER BY t2018.geozone_id
+        ), stats_parcelles_remises_en_etat_2024 AS (
+         SELECT t2021.geozone_id,
+            count(DISTINCT t2021.id_parcellaire) AS parcelles_remises_en_etat_2024_vs_2021
+           FROM analytics.stats_detections_analysis t2021
+          WHERE t2021.batch_year = 2021::numeric AND t2021.id_parcellaire IS NOT NULL AND NOT (EXISTS ( SELECT 1
+                   FROM analytics.stats_detections_analysis t2024
+                  WHERE t2024.batch_year = 2024::numeric AND t2024.id_parcellaire = t2021.id_parcellaire AND t2024.geozone_id = t2021.geozone_id))
+          GROUP BY t2021.geozone_id
+          ORDER BY t2021.geozone_id
+        )
+ SELECT fgr.department_name,
+    fgr.geozone_code,
+    stats_pv_2021_2023.geozone_id,
+    stats_pv_2021_2023.nb_pv_2021_2023 AS nb_pv_period,
+    stats_parcelles_cabanisees_2021.situation_parcelles_cabanisees_2021 AS situation_parcelles_cabanisees_debut_periode,
+    stats_parcelles_cabanisees_2024.situation_parcelles_cabanisees_2024 AS situation_parcelles_cabanisees_fin_periode,
+    stats_nouvelles_parcelles_cabanisees_2024.nouvelles_parcelles_cabanisees_2024_vs_2021 AS situation_nouvelles_parcelles_cabanisees_periode,
+    stats_parcelles_remises_en_etat_2024.parcelles_remises_en_etat_2024_vs_2021 AS situation_parcelles_remises_en_etat_periode,
+        CASE
+            WHEN fgr.department_name::text = 'gard'::text THEN 'lutte avec données aigle'::text
+            WHEN fgr.id = ANY (ARRAY[278::bigint, 171::bigint, 259::bigint, 195::bigint, 261::bigint, 138::bigint, 313::bigint, 281::bigint, 330::bigint, 118::bigint, 255::bigint, 111::bigint, 288::bigint, 226::bigint, 248::bigint]) THEN 'pas d engagement'::text
+            ELSE 'lutte avec aigle'::text
+        END AS groupe,
+    '2021-2023'::text AS periode,
+    spa.nb_parcels,
+    spa.nb_parcels_in_ze
+   FROM stats_pv_2021_2023
+     LEFT JOIN stats_parcelles_cabanisees_2021 ON stats_pv_2021_2023.geozone_id = stats_parcelles_cabanisees_2021.geozone_id
+     LEFT JOIN stats_parcelles_cabanisees_2024 ON stats_pv_2021_2023.geozone_id = stats_parcelles_cabanisees_2024.geozone_id
+     LEFT JOIN stats_nouvelles_parcelles_cabanisees_2024 ON stats_pv_2021_2023.geozone_id = stats_nouvelles_parcelles_cabanisees_2024.geozone_id
+     LEFT JOIN stats_parcelles_remises_en_etat_2024 ON stats_pv_2021_2023.geozone_id = stats_parcelles_remises_en_etat_2024.geozone_id
+     LEFT JOIN analytics.stats_mapping_preprod_geozone_id fgr ON stats_pv_2021_2023.geozone_id = fgr.id::numeric
+     LEFT JOIN analytics.stats_parcels_analysis spa ON stats_pv_2021_2023.geozone_id = spa.geozone_id::numeric
+  WHERE stats_parcelles_cabanisees_2021.situation_parcelles_cabanisees_2021 IS NOT NULL
+UNION
+ SELECT fgr.department_name,
+    fgr.geozone_code,
+    stats_pv_2018_2020.geozone_id,
+    stats_pv_2018_2020.nb_pv_2018_2020 AS nb_pv_period,
+    stats_parcelles_cabanisees_2018.situation_parcelles_cabanisees_2018 AS situation_parcelles_cabanisees_debut_periode,
+    stats_parcelles_cabanisees_2021.situation_parcelles_cabanisees_2021 AS situation_parcelles_cabanisees_fin_periode,
+    stats_nouvelles_parcelles_cabanisees_2021.nouvelles_parcelles_cabanisees_2021_vs_2018 AS situation_nouvelles_parcelles_cabanisees_periode,
+    stats_parcelles_remises_en_etat_2021.parcelles_remises_en_etat_2021_vs_2018 AS situation_parcelles_remises_en_etat_periode,
+        CASE
+            WHEN fgr.id = ANY (ARRAY[278::bigint, 171::bigint, 259::bigint, 195::bigint, 261::bigint, 138::bigint, 313::bigint, 281::bigint, 330::bigint, 118::bigint, 255::bigint, 111::bigint, 288::bigint, 226::bigint, 248::bigint]) THEN 'pas d engagement'::text
+            ELSE 'lutte sans outils'::text
+        END AS groupe,
+    '2018-2020'::text AS periode,
+    spa.nb_parcels,
+    spa.nb_parcels_in_ze
+   FROM stats_pv_2018_2020
+     LEFT JOIN stats_parcelles_cabanisees_2018 ON stats_pv_2018_2020.geozone_id = stats_parcelles_cabanisees_2018.geozone_id
+     LEFT JOIN stats_parcelles_cabanisees_2021 ON stats_pv_2018_2020.geozone_id = stats_parcelles_cabanisees_2021.geozone_id
+     LEFT JOIN stats_nouvelles_parcelles_cabanisees_2021 ON stats_pv_2018_2020.geozone_id = stats_nouvelles_parcelles_cabanisees_2021.geozone_id
+     LEFT JOIN stats_parcelles_remises_en_etat_2021 ON stats_pv_2018_2020.geozone_id = stats_parcelles_remises_en_etat_2021.geozone_id
+     LEFT JOIN analytics.stats_mapping_preprod_geozone_id fgr ON stats_pv_2018_2020.geozone_id = fgr.id::numeric
+     LEFT JOIN analytics.stats_parcels_analysis spa ON stats_pv_2018_2020.geozone_id = spa.geozone_id::numeric
+  WHERE stats_parcelles_cabanisees_2018.situation_parcelles_cabanisees_2018 IS NOT NULL;
